9fcfc4912e967af337ea833f12c045ec8e23a715
alex_zhooson <861077046@qq.com>
2021-08-23 16:50:49 +0800

    fix: 层级

--- vue-cli3-admin-3434.js
+++ vue-cli3-admin-3434.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 
 //指定启动服务器到哪个文件夹，我这边指的是dist文件夹
-app.use(express.static('./dist'));
+app.use(express.static('./'));
 
 // 监听端口为3000
 var server = app.listen(3434, function() {
-    console.info('http://localhost:3434')
-})
+  console.info('http://localhost:3434');
+});
